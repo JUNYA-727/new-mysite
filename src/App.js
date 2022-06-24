b16ccd567e0d3b93f0646427e700158f1f950d31
JUNYA-727 <k.junya0727@icloud.com>
2022-06-24 21:27:17 +0900

    master

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/** @format */
+/*
+・メールのお問い合わせ機能の追加
+・tailwindでさらに色をつける
+・画像を加えてもいいかも
+・パソコンとスマホ用に適したサイズで返すようにする。
+*/
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div id="header" style={{}}>
+        hello
+      </div>
+    </>
+  );
+}
+
+export default App;
